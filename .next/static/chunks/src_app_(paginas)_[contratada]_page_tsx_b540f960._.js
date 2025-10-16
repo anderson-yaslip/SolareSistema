ddbe717bbb91c9899e1f9687886703b55d760e85
wesley-yaslip <wesley@yaslip.com.br>
2025-10-16 10:08:54 -0300

    removendo build corretamente

--- .next/static/chunks/src_app_(paginas)_[contratada]_page_tsx_b540f960._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_3a0ecf6f._.css",
-  "static/chunks/node_modules_114843f0._.js",
-  "static/chunks/src_f67cf4b7._.js"
-],
-    source: "dynamic"
-});
